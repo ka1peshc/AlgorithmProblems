b1af3bd0125e274d22c31add37b19982743b9f0b
ka1peshc <kalpesh111997@gmail.com>
2021-09-24 23:26:32 +0530

    Check permutation for a given string

--- AlgorithmProblem/Program.cs
+++ AlgorithmProblem/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace AlgorithmProblem
 {
     class Program
@@ -7,6 +7,15 @@ namespace AlgorithmProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //StringPermutation sp = new StringPermutation();
+            string s = "mango";
+            int n = s.Length;
+            StringPermutation.permute(s, 0, n - 1);
+            //List<string> strList = StringPermutation.permutationNames;
+            //foreach(var i in strList)
+            //{
+            //    Console.WriteLine(i);
+            //}
         }
     }
 }
--- AlgorithmProblem/StringPermutation.cs
+++ AlgorithmProblem/StringPermutation.cs
@@ -6,5 +6,36 @@ namespace AlgorithmProblem
 {
     class StringPermutation
     {
+        internal static List<string> permutationNames;
+        
+        public static void permute(String s, int startIndex, int endIndex)
+        {
+            if(startIndex == endIndex)
+            {
+                Console.WriteLine(s);
+                
+            }
+            else
+            {
+                for(int i = startIndex; startIndex < endIndex; startIndex++)
+                {
+                    s = swaping(s, startIndex, i);
+                    permute(s, startIndex + 1, endIndex);
+                    s = swaping(s, startIndex, i);
+                }
+            }
+            
+        }
+        public static String swaping(String s, int i, int j)
+        {
+            char temp;
+            char[] charArr = s.ToCharArray();
+            temp = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+            string resultString = new string(charArr);
+            
+            return resultString;
+        }
     }
 }
